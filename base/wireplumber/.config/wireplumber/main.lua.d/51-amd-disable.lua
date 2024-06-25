30dd55bcf7779305e25116760bf3cb3f885226ac
anzix <givefly3@gmail.com>
2024-06-25 12:18:22 +0500

    Немного изменил устаревший lua конфиг для wireplumber

--- base/wireplumber/.config/wireplumber/main.lua.d/51-amd-disable.lua
+++ base/wireplumber/.config/wireplumber/main.lua.d/51-amd-disable.lua
@@ -1,12 +1,15 @@
 rule = {
-  matches = {
-    {
-      { "device.name", "equals", "alsa_card.pci-0000_03_00.1" },
-    },
-  },
-  apply_properties = {
-    ["device.disabled"] = true,
-  },
+	matches = {
+		{
+			{ "device.name", "equals", "alsa_card.pci-0000_03_00.1" },
+		},
+		{
+			{ "device.name", "equals", "alsa_output.pci-0000_03_00.1.hdmi-stereo-extra5" },
+		},
+	},
+	apply_properties = {
+		["device.disabled"] = true,
+	},
 }
 
 table.insert(alsa_monitor.rules, rule)
